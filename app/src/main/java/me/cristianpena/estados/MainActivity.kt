c528a9107023b7b5908fcf77e182f747ec7d24bd
Your Name <you@example.com>
2026-09-23 15:39:18 -0500

    contador funcional

--- app/src/main/java/me/cristianpena/estados/MainActivity.kt
+++ app/src/main/java/me/cristianpena/estados/MainActivity.kt
@@ -18,23 +18,9 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
+            StateScreen()
 
-            }
         }
     }
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
 }
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    EstadosTheme {
-        Greeting("Android")
-    }
-}
